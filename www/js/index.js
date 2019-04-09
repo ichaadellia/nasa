cb3f3c07b3d3070940b08513b432ce6703c408fd
ichaadellia <48471700+ichaadellia@users.noreply.github.com>
2019-04-09 14:46:00 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=reC17I2uaHw9n5LBTKL3MffVhPsAvNN31iTTqHyu";
         
         $("#loadImage").click(function(){
             $.ajax({
